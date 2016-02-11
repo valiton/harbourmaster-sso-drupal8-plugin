6fa88f0f5092a029c0154919151cb733f44ec9a7
Patrick Durold <patrick.durold@burda.com>
2016-02-11 02:55:20 +0100

    Auth Provider scaffold

--- src/Authentication/Provider/Token.php
+++ src/Authentication/Provider/Token.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: patrick.durold
- * Date: 11.02.16
- * Time: 02:21
- */
 
 namespace hms\src\Authentication\Provider;
 
+use Drupal\user\Authentication\Provider\Cookie;
 
-class Token extends Coo {
+class Token extends Cookie {
 
 }
