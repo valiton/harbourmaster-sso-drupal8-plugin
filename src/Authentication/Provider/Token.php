70c989a241e9e1e5d408d7c8b2bb5d02cbb3e8ab
Patrick Durold <patrick.durold@burda.com>
2016-02-11 02:52:10 +0100

    Auth Provider scaffold

---
+++ src/Authentication/Provider/Token.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patrick.durold
+ * Date: 11.02.16
+ * Time: 02:21
+ */
+
+namespace hms\src\Authentication\Provider;
+
+
+class Token extends Coo {
+
+}
